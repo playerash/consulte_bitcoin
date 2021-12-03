aaa3cd3d9651d7dee9358f9a73305df0d095d7d9
playerash <50275426+playerash@users.noreply.github.com>
2021-12-03 12:00:12 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Consulta_bitcoin
+ 
---
+++ lib/app/app_widget.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:preco_bitcoin/app/home_page/home_page.dart';
+
+class AppWidget extends StatelessWidget {
+  const AppWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+    );
+  }
+}
